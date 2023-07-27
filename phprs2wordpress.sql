12ba0877d1350ea69488f5cb1250dc24d270249b
Martin Skalický <skalicky@modernalt.eu>
2023-07-27 14:38:38 +0200

    Update phprs2wordpress.sql
    Update 0.2

--- phprs2wordpress.sql
+++ phprs2wordpress.sql
@@ -21,14 +21,15 @@ SELECT
 FROM archiv.rs_user AS rs_user;
 
 -- Step 2: Import data from "archiv.rs_clanky" and "archiv.rs_user" to "staryweb.wp_posts" (posts and post authors).
-INSERT INTO staryweb.wp_posts (post_date, post_date_gmt, post_author, post_content, post_title, post_excerpt)
+INSERT INTO staryweb.wp_posts (post_date, post_date_gmt, post_author, post_content, post_title, post_excerpt, post_name)
 SELECT 
     DATE_SUB(datum, INTERVAL 2 HOUR) AS post_date,
     DATE_SUB(datum, INTERVAL 2 HOUR) AS post_date_gmt,
     wp_users.ID AS post_author,
-    CONCAT(uvod, '<br>', text, '<br><br>', autor) AS post_content,
+    text AS post_content,
     titulek AS post_title,
-    uvod AS post_excerpt
+    uvod AS post_excerpt,
+    titulek AS post_name
 FROM archiv.rs_clanky
 JOIN archiv.rs_user ON archiv.rs_clanky.autor = archiv.rs_user.idu
 JOIN staryweb.wp_users AS wp_users ON rs_user.jmeno = wp_users.display_name;
@@ -57,5 +58,39 @@ JOIN archiv.rs_topic ON archiv.rs_clanky.tema = archiv.rs_topic.idt
 JOIN staryweb.wp_terms ON staryweb.wp_terms.name = archiv.rs_topic.nazev
 JOIN staryweb.wp_term_taxonomy ON staryweb.wp_terms.term_id = staryweb.wp_term_taxonomy.term_id
 WHERE staryweb.wp_term_taxonomy.taxonomy = 'post_tag';
+
+
+-- Rewrite without diacritics
+-- Drop the existing function (if it already exists)
+DROP FUNCTION IF EXISTS RemoveDiacritics;
+
+-- Create the updated function
+DELIMITER //
+CREATE FUNCTION RemoveDiacritics(inputText VARCHAR(255)) RETURNS VARCHAR(255)
+DETERMINISTIC
+BEGIN
+    SET @outputText = inputText;
+    SET @diacritics = 'ÁÄČĎÉĚÍĹĽŇÓÖŘŠŤÚŮÝŽáäčďéěíĺľňóöřšťúůýž';
+    SET @replaceChars = 'AACDEEILLNOORSTUUYZaacdeeillnoorstuuyz';
+    SET @diacriticIndex = 1;
+    SET @diacriticCount = CHAR_LENGTH(@diacritics);
+
+    WHILE @diacriticIndex <= @diacriticCount DO
+        SET @diacriticChar = SUBSTRING(@diacritics, @diacriticIndex, 1);
+        SET @replaceChar = SUBSTRING(@replaceChars, @diacriticIndex, 1);
+        SET @outputText = REPLACE(@outputText, @diacriticChar, @replaceChar);
+        SET @diacriticIndex = @diacriticIndex + 1;
+    END WHILE;
+
+    SET @outputText = REPLACE(@outputText, ' ', '-'); -- Replace spaces with "-"
+
+    RETURN LOWER(@outputText); -- Convert the output to lowercase
+END//
+DELIMITER ;
+
+
+-- Step 2: Use the function to update the "slug" column in "wp_terms" table
+UPDATE wp_terms SET slug = RemoveDiacritics(slug);
+UPDATE wp_posts SET post_name = RemoveDiacritics(post_name);
 -- Created by Martin J Skalicky, 2023
 -- https://github.com/maskalix/phprs-to-wordpress-db-transfer/
